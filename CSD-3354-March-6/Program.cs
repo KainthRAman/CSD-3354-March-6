c4c1d6fc4f88f587825d03d2a379b35a21692ef6
visualstudio <visualstudio@122-PC36>
2019-03-06 10:29:16 -0500

    CSD_3354_lab1_c0723796_c07307263

--- CSD-3354-March-6/Program.cs
+++ CSD-3354-March-6/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace CSD_3354_March_6
 {
-    class Program
+    public class DelegateExercises
     {
-        static void Main(string[] args)
+        public delegate void MyDelegate();
+        void method1()
         {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(method1);
+            myDelegate();
         }
     }
+
+    public class Raman
+    {
+        public static void Main()
+        {
+            var de = new DelegateExercises();
+            de.Method2();
+      
+        }
+
+    }
 }
+    
+        
+           
